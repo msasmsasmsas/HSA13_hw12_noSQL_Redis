f10aed17b6d3e3e6c7568aa4115b82f4ebd826ee
HSA13_hw4_Stress_Testing_Approaches_and_tools <msas@ukr.net>
2025-02-23 22:47:16 +0200

    2

--- load-test/load_test_redis_aof.sh
+++ load-test/load_test_redis_aof.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-siege -c 1 -r 10 -H "Content-Type: application/json" -f ./urls_redis2.txt -b
+siege -c 1 -r 10 -H "Content-Type: application/json" -f ./urls_redis_aof.txt -b
--- load-test/load_test_redis_rdb_dequeue.sh
+++ load-test/load_test_redis_rdb_dequeue.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-siege -c 1 -r 10 -H "Content-Type: application/json" -f ./urls_redis1_dequeue.txt -b
+siege -c 1 -r 10 -H "Content-Type: application/json" -f ./urls_redis_rdb_dequeue.txt -b
